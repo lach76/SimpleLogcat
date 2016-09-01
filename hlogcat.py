d820ac3f74f21cad43d5edf7b4171d259ff81c5b
lachryn <lach76@gmail.com>
2016-09-01 23:41:03 +0900

    wip - change somecode

--- hlogcat.py
+++ hlogcat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-
-#   Code referenced colored_logcat.py (https://github.com/marshall/logcat-color)
+#
+#   Code referenced colored_logcat.py (http://jsharkey.org/blog/2009/04/22/modifying-the-android-logcat-stream-for-full-color-debugging/)
 #   Code referenced stackoverflow (http://stackoverflow.com/questions/11524586/accessing-logcat-from-android-via-python)
 
 import os, sys, re, StringIO
@@ -13,6 +13,10 @@ import threading
 import datetime
 
 import tty
+import json
+
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+
 def getch():
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
@@ -23,6 +27,25 @@ def getch():
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
 
     return ch
+
+def format(fg=None, bg=None, bright=False, bold=False, dim=False, reset=False):
+    codes = []
+    if reset:
+        codes.append("0")
+    else:
+        if not fg is None: codes.append("3%d" % (fg))
+        if not bg is None:
+            if not bright:
+                codes.append("4%d" % (bg))
+            else:
+                codes.append("10%d" % (bg))
+        if bold:
+            codes.append("1")
+        elif dim:
+            codes.append("2")
+        else:
+            codes.append("22")
+    return "\033[%sm" % (";".join(codes))
 
 
 class AsynchronousFileReader(threading.Thread):
@@ -40,114 +63,109 @@ class AsynchronousFileReader(threading.Thread):
     def eof(self):
         return not self.is_alive() and self._queue.empty()
 
-def format(fg=None, bg=None, bright=False, bold=False, dim=False, reset=False):
-    codes = []
-    if reset: codes.append("0")
-    else:
-        if not fg is None: codes.append("3%d" % (fg))
-        if not bg is None:
-            if not bright: codes.append("4%d" % (bg))
-            else: codes.append("10%d" % (bg))
-        if bold: codes.append("1")
-        elif dim: codes.append("2")
-        else: codes.append("22")
-    return "\033[%sm" % (";".join(codes))
-    
+class LogcatPrint:
+    mLastUsed = [GREEN, YELLOW, BLUE, MAGENTA, CYAN, BLACK]
 
-def indent_wrap(message, indent=0, width=80):
-    wrap_area = width - indent
-    messagebuf = StringIO.StringIO()
-    current = 0
-    while current < len(message):
-        next = min(current + wrap_area, len(message))
-        messagebuf.write(message[current:next])
-        if next < len(message):
-            messagebuf.write("\n%s" % (" " * indent))
-        current = next
-    return messagebuf.getvalue()
+    def __init__(self):
+        self.mRegexTag = re.compile(
+            "^([0-9][0-9]-[0-9][0-9]) ([0-9][0-9]:[0-9][0-9]:[0-9][0-9]\.[0-9][0-9][0-9])\s+(\d+)\s+(\d+) ([A-Z]) ([^:]*)[: +](.*)$")
 
-def allocate_color(tag):
-    # this will allocate a unique format for the given tag
-    # since we dont have very many colors, we always keep track of the LRU
-    if not tag in KNOWN_TAGS:
-        KNOWN_TAGS[tag] = LAST_USED[0]
-    color = KNOWN_TAGS[tag]
-    LAST_USED.remove(color)
-    LAST_USED.append(color)
-    return color
-
-def allocate_color_pid(pid):
-    # this will allocate a unique format for the given tag
-    # since we dont have very many colors, we always keep track of the LRU
-    color = LAST_USED[int(pid) % 6]
-    return color
+        tagtypeWidth = 3
+        self.mTagWidth = 25
+        self.mTagTypes = {
+            "V": "%s%s%s " % (format(fg=WHITE, bg=BLACK), "V".center(tagtypeWidth), format(reset=True)),
+            "D": "%s%s%s " % (format(fg=BLACK, bg=BLUE), "D".center(tagtypeWidth), format(reset=True)),
+            "I": "%s%s%s " % (format(fg=BLACK, bg=GREEN), "I".center(tagtypeWidth), format(reset=True)),
+            "W": "%s%s%s " % (format(fg=BLACK, bg=YELLOW), "W".center(tagtypeWidth), format(reset=True)),
+            "E": "%s%s%s " % (format(fg=BLACK, bg=RED), "E".center(tagtypeWidth), format(reset=True)),
+            "F": "%s%s%s " % (format(fg=BLACK, bg=RED), "F".center(tagtypeWidth), format(reset=True)),
+        }
 
 
-def print_line(line):
-    match = retag.match(line)
-    if not match is None:
-        date, time, pid, tid, tagtype, tag, message = match.groups()
-        linebuf = StringIO.StringIO()
+    def indent_wrap(self, message, indent=0, width=80):
+        wrap_area = width - indent
+        messagebuf = StringIO.StringIO()
+        current = 0
+        while current < len(message):
+            next = min(current + wrap_area, len(message))
+            messagebuf.write(message[current:next])
+            if next < len(message):
+                messagebuf.write("\n%s" % (" " * indent))
+            current = next
+        return messagebuf.getvalue()
 
-        # center process info
-        #owner = owner.strip()
-        tag = tag.strip()
-        color = allocate_color_pid(pid)
-		
-        if len(pid) == 2: pid = " " + pid
-        if len(pid) == 1: pid = "  " + pid
-        if len(tid) == 2: tid += " "
-        if len(tid) == 1: tid += "  "
-		
-        time = time + ' ' + pid + '/' + tid
-        if color == BLACK:
-            linebuf.write("%s%s%s " % (format(bg=WHITE, fg=color, dim=False), time, format(reset=True)))
-	else:
-	    linebuf.write("%s%s%s " % (format(fg=color, dim=False), time, format(reset=True)))
-	
-		
-        # right-align tag title and allocate color if needed
-         
-        tag = tag[-TAG_WIDTH:].rjust(TAG_WIDTH)
+    def allocate_color_by_pid(self, pid):
+        # this will allocate a unique format for the given tag
+        # since we dont have very many colors, we always keep track of the LRU
+        color = self.mLastUsed[int(pid) % 6]
+        return color
 
-        if color == BLACK:
-	    linebuf.write("%s%s %s" % (format(bg=WHITE, fg=color, dim=False), tag, format(reset=True)))
-        else:
-	    linebuf.write("%s%s %s" % (format(fg=color, dim=False), tag, format(reset=True)))
+    def regex_calc(self, line):
+        return self.mRegexTag.match(line)
 
-        # write out tagtype colored edge
-        if not tagtype in TAGTYPES: return
-        linebuf.write(TAGTYPES[tagtype])
+    def printlog(self, line):
+        match = self.regex_calc(line)
+        if not match is None:
+            date, time, pid, tid, tagtype, tag, message = match.groups()
+            linebuf = StringIO.StringIO()
 
-        # insert line wrapping as needed
-        if tagtype == "E":
-            if len(sys.argv) == 2:
-                message = message.replace(sys.argv[1], highlight_err)
-            linebuf.write("%s%s%s" % (format(fg=RED, bold=True), message, format(reset=True)))
-        #elif tagtype == "W":
-        #    linebuf.write("%s%s%s" % (format(fg=YELLOW, bold=True, dim=True), message, format(reset=True)))
-        else:
-            if len(sys.argv) == 2:
-                message = message.replace(sys.argv[1], highlight)
-            linebuf.write(message)
+            # center process info
+            # owner = owner.strip()
+            tag = tag.strip()
+            color = self.allocate_color_by_pid(pid)
 
-        #message = indent_wrap(message, HEADER_SIZE, WIDTH)
+            if len(pid) == 2: pid = " " + pid
+            if len(pid) == 1: pid = "  " + pid
+            if len(tid) == 2: tid += " "
+            if len(tid) == 1: tid += "  "
 
-        line = linebuf.getvalue()
+            time = time + ' ' + pid + '/' + tid
+            if color == BLACK:
+                linebuf.write("%s%s%s " % (format(bg=WHITE, fg=color, dim=False), time, format(reset=True)))
+            else:
+                linebuf.write("%s%s%s " % (format(fg=color, dim=False), time, format(reset=True)))
 
-    print line
+            # right-align tag title and allocate color if needed
+
+            tag = tag[-self.mTagWidth:].rjust(self.mTagWidth)
+
+            if color == BLACK:
+                linebuf.write("%s%s %s" % (format(bg=WHITE, fg=color, dim=False), tag, format(reset=True)))
+            else:
+                linebuf.write("%s%s %s" % (format(fg=color, dim=False), tag, format(reset=True)))
+
+            # write out tagtype colored edge
+            if not tagtype in self.mTagTypes: return
+            linebuf.write(self.mTagTypes[tagtype])
+
+            # insert line wrapping as needed
+            if tagtype == "E":
+                linebuf.write("%s%s%s" % (format(fg=RED, bold=True), message, format(reset=True)))
+            # elif tagtype == "W":
+            #    linebuf.write("%s%s%s" % (format(fg=YELLOW, bold=True, dim=True), message, format(reset=True)))
+            else:
+                linebuf.write(message)
+
+            line = linebuf.getvalue()
+
+        print line
+
 
 mStillRunning = True
 mPauseLog = False
 
+
 def print_title(data):
     print "%s%s%s" % (format(fg=YELLOW, bold=True), data, format(reset=True))
+
 
 def print_text(data):
     print "%s%s%s" % (format(fg=WHITE, bold=False), data, format(reset=True))
 
+
 def print_notice(data):
     print "%s%s%s" % (format(fg=GREEN, bold=False), data, format(reset=True))
+
 
 def print_help():
     print_title("----------   Help  ----------")
@@ -167,13 +185,15 @@ def print_help():
     print_text("unany      : unset filter for any : unany XXXX")
     print_text("")
 
-FilterInfo = {"mode":True, "pid":{}, "module":{}, "any":{}}
+
+FilterInfo = {"mode": True, "pid": {}, "module": {}, "any": {}}
+
 
 def run_filter_command(command_list):
     global FilterInfo
 
     filter_mode = FilterInfo["mode"]
-    filter_module = FilterInfo["module"]    
+    filter_module = FilterInfo["module"]
     filter_pid = FilterInfo["pid"]
     filter_any = FilterInfo["any"]
 
@@ -186,7 +206,7 @@ def run_filter_command(command_list):
     args = arg.split(':')
 
     arg1 = ''.join(args[0].split())
-    
+
     if command == 'mode':
         if arg == 'mask':
             filter_mode = True
@@ -204,20 +224,21 @@ def run_filter_command(command_list):
     else:
         if len(args) == 2:
             if command == 'module':
-                filter_module[arg1] = args[1] 
+                filter_module[arg1] = args[1]
             elif command == 'pid':
                 filter_pid[arg1] = args[1]
             elif command == 'any':
                 filter_any[arg1] = args[1]
-   
+
     print_title("Filter is set")
     print_filter_information()
+
 
 def print_filter_information():
     global FilterInfo
 
     filter_mode = FilterInfo["mode"]
-    filter_module = FilterInfo["module"]    
+    filter_module = FilterInfo["module"]
     filter_pid = FilterInfo["pid"]
     filter_any = FilterInfo["any"]
 
@@ -238,6 +259,7 @@ def print_filter_information():
     for key, value in filter_any.items():
         print_text("-- [%s] : [%s]" % (key, value))
 
+
 def userInputThreadFunc():
     global mStillRunning
     global mPauseLog
@@ -249,12 +271,12 @@ def userInputThreadFunc():
 
         mPauseLog = True
         print_notice("LOG is PAUSED")
-        if (ch == chr(3)):      # Ctrl + C
+        if (ch == chr(3)):  # Ctrl + C
             print "Exit HLogcat"
             mStillRunning = False
         elif ch == '?':
             print_help()
-        elif (ch == '/') or (ch == chr(0x0d)):    # Command Input mode
+        elif (ch == '/') or (ch == chr(0x0d)):  # Command Input mode
             if ch == chr(0x0d):
                 print_help()
             command = raw_input("/ ")
@@ -276,11 +298,12 @@ def userInputThreadFunc():
         print_notice("LOG is RESUMED")
         mPauseLog = False
 
+
 def isPrintable(pid, tag, tagtype, message):
     global FilterInfo
 
     filter_mode = FilterInfo["mode"]
-    filter_module = FilterInfo["module"]    
+    filter_module = FilterInfo["module"]
     filter_pid = FilterInfo["pid"]
     filter_any = FilterInfo["any"]
 
@@ -293,7 +316,7 @@ def isPrintable(pid, tag, tagtype, message):
     tagtype = ''.join(tag.split())
     message = ''.join(message.split())
     isFound = False;
-    
+
     if filter_module.has_key(tag):
         isFound = True
     if filter_pid.has_key(pid):
@@ -308,11 +331,12 @@ def isPrintable(pid, tag, tagtype, message):
         isFound = not isFound
     return isFound
 
-import json
+
 def save_filter_info():
     global FilterInfo
     with open("hlogcat.json", "w") as file:
         json.dump(FilterInfo, file)
+
 
 def load_filter_info():
     global FilterInfo
@@ -320,41 +344,13 @@ def load_filter_info():
         with open("hlogcat.json", "r") as file:
             FilterInfo = json.load(file)
 
+
 if __name__ == "__main__":
     # init terminal
-    data = fcntl.ioctl(sys.stdout.fileno(), termios.TIOCGWINSZ, '1234')
-    HEIGHT, WIDTH = struct.unpack('hh',data)
-    BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
-
-    LAST_USED = [GREEN,YELLOW,BLUE,MAGENTA,CYAN,BLACK]
-    KNOWN_TAGS = {
-        "dalvikvm": BLUE,
-        "Process": BLUE,
-        "ActivityManager": CYAN,
-        "ActivityThread": CYAN,
-    }
-
-    TAGTYPE_WIDTH = 3
-    TAG_WIDTH = 25
-    PROCESS_WIDTH = 21 #8 # 8 or -1
-    HEADER_SIZE = TAGTYPE_WIDTH + 1 + TAG_WIDTH + 1 + PROCESS_WIDTH + 1
-
-    TAGTYPES = {
-        "V": "%s%s%s " % (format(fg=WHITE, bg=BLACK), "V".center(TAGTYPE_WIDTH), format(reset=True)),
-        "D": "%s%s%s " % (format(fg=BLACK, bg=BLUE), "D".center(TAGTYPE_WIDTH), format(reset=True)),
-        "I": "%s%s%s " % (format(fg=BLACK, bg=GREEN), "I".center(TAGTYPE_WIDTH), format(reset=True)),
-        "W": "%s%s%s " % (format(fg=BLACK, bg=YELLOW), "W".center(TAGTYPE_WIDTH), format(reset=True)),
-        "E": "%s%s%s " % (format(fg=BLACK, bg=RED), "E".center(TAGTYPE_WIDTH), format(reset=True)),
-        "F": "%s%s%s " % (format(fg=BLACK, bg=RED), "F".center(TAGTYPE_WIDTH), format(reset=True)),
-    }
-    retag = re.compile("^([0-9][0-9]-[0-9][0-9]) ([0-9][0-9]:[0-9][0-9]:[0-9][0-9]\.[0-9][0-9][0-9])\s+(\d+)\s+(\d+) ([A-Z]) ([^:]*)[: +](.*)$")
-    pid_out = '0'
-    pid_btld = '0'
-
     load_filter_info()
     print_filter_information()
 
-
+    logPrint = LogcatPrint()
     # You'll need to add any command line arguments here.
     process = subprocess.Popen(['adb', 'logcat'], stdout=subprocess.PIPE)
 
@@ -368,21 +364,22 @@ if __name__ == "__main__":
 
     # Check the queues if we received some output (until there is nothing more to get).
     mStillRunning = True
-    mPauseLog = False;
+    mPauseLog = False
     try:
         while mStillRunning and not stdout_reader.eof():
             while not stdout_queue.empty() and not mPauseLog:
                 printable = False
 
                 line = stdout_queue.get()
+                print line
                 # TODO: Add filter.
-                match = retag.match(line)
+                match = logPrint.regex_calc(line)
                 if not match is None:
                     _date, _time, _pid, _tid, _tagtype, _tag, _message = match.groups()
                     if isPrintable(_pid.lower(), _tag.lower(), _tagtype.lower(), _message.lower()):
-                        print_line(line)
+                        logPrint.printlog(line)
                 else:
-                    print_line(line)
+                    logPrint.printlog(line)
             time.sleep(0.1)
 
     except KeyboardInterrupt:
